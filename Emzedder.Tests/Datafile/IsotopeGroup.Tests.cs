1fe4fb3339b2c65f91e3602ec8ade9b2bea81ef1
James Ault <james@helmfarm.com>
2024-11-01 11:37:57 +0000

    create IsotopeGroup object

--- Emzedder.Tests/Datafile/IsotopeGroup.Tests.cs
+++ Emzedder.Tests/Datafile/IsotopeGroup.Tests.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Emzedder.Datafile;
 
 namespace Emzedder.Tests.Datafile
 {
-    internal class IsotopeGroup
+    public class IsotopeGroupTests
+
     {
+        [Fact]
+        public void Constructor_SetsChargeProperty()
+        {
+            int expectedCharge = 2;
+
+            var isotopeGroup = new IsotopeGroup(expectedCharge);
+
+            Assert.Equal(expectedCharge, isotopeGroup.Charge);
+        }
+        [Fact]
+        public void AddIsotope_CorrectlyAddsIsotopeToList()
+        {
+            //test add and get separately?
+            var expectedLength = 1;
+
+            var dp = new MSDatapoint() { Mz = 1.234, Intensity = 1234 };
+
+            var group = new IsotopeGroup(2);
+
+            group.AddIsotope(dp);
+
+            var isotopes = group.GetIsotopeGroup();
+
+            Assert.Equal(expectedLength, isotopes.Length);
+
+            Assert.Equal(dp, isotopes[0]);
+
+        }
+
     }
 }
---
+++ Emzedder/Datafile/IsotopeGroup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emzedder.Datafile
+{
+    internal class IsotopeGroup
+    {
+        private List<MSDatapoint> IsotopePeaks = [];
+        public int Charge { get; init; }
+
+        public IsotopeGroup(int charge)
+        {
+            Charge = charge;
+        }
+        public void AddIsotope(MSDatapoint isotope)
+        {
+            IsotopePeaks.Add(isotope);
+        }
+        public MSDatapoint[] GetIsotopeGroup()
+        {
+            return IsotopePeaks.ToArray();
+        }
+
+    }
+}
