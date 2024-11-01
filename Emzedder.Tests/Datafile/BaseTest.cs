d2f7bcdbc4cc7fd8029ac02e8b5287b384ca69cc
James Ault <james@helmfarm.com>
2024-11-01 11:41:33 +0000

    add BaseTest for Datafile and Spectrum tests

--- Emzedder.Tests/Datafile/BaseTest.cs
+++ Emzedder.Tests/Datafile/BaseTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Emzedder.Datafile;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,18 +9,35 @@ namespace Emzedder.Tests.Datafile
 {
     public class BaseTest
     {
-        protected readonly string _invalidFilePath;
-        protected readonly string _validFilePath;
-        protected readonly string _invalidRawFile;
-
-
-        protected BaseTest()
+        protected readonly string _invalidFilePath = @"C:\Users\runra\source\repos\Emzedder\Emzedder\TestRawDatafiles\invalid.raw";
+        protected readonly string _validFilePath = @"C:\Users\runra\source\repos\Emzedder\Emzedder\TestRawDatafiles\1.raw";
+        protected readonly string _invalidRawFile = @"C:\Users\runra\source\repos\Emzedder\Emzedder\TestRawDatafiles\NotAValidRawFile.raw";
+        protected readonly string _msSpectrumDataCsv = @"C:\Users\runra\source\repos\Emzedder\Emzedder.Tests\TestData\MSTestData.csv";
+        protected readonly int _scanNumber = 30749;
+        protected MSDatapoint[] ExpectedMsSpectrumDatapoints { get; private set; }
+        public BaseTest()
         {
-            _validFilePath = @"C:\Users\runra\source\repos\Emzedder\Emzedder\TestRawDatafiles\invalid.raw";
+            BuildTestMsSpectrum();
+        }
+        private void BuildTestMsSpectrum()
+        {
+            var datapoints = new List<MSDatapoint>();
 
-            _validFilePath = @"C:\Users\runra\source\repos\Emzedder\Emzedder\TestRawDatafiles\1.raw";
+            foreach (var line in File.ReadLines(_msSpectrumDataCsv))
+            {
+                if (string.IsNullOrWhiteSpace(line) || !line.Contains(","))
+                    continue;
 
-            _invalidRawFile = @"C:\Users\runra\source\repos\Emzedder\Emzedder\TestRawDatafiles\NotAValidRawFile.raw";
+                var parts = line.Split(',');
+                if (parts.Length == 2 &&
+                    double.TryParse(parts[0], out double mz) &&
+                    double.TryParse(parts[1], out double intensity))
+                {
+                    datapoints.Add(new MSDatapoint { Mz = Math.Round(mz,4), Intensity = Math.Round(intensity,4) });
+                }
+            }
+
+            ExpectedMsSpectrumDatapoints = [.. datapoints];
         }
     }
 }
--- Emzedder.Tests/Datafile/ThermoDatafile.Tests.cs
+++ Emzedder.Tests/Datafile/ThermoDatafile.Tests.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Emzedder.Datafile;
 
-using Emzedder.Datafile;
-using Moq;
-
-using ThermoFisher.CommonCore.Data.Interfaces;
 
 
 namespace Emzedder.Tests.Datafile
